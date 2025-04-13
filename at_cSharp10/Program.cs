16648e41700b59a82da85cf53751c0ad3ade6675
Isabella Mayer <bella@MacBook-Pro-de-Isabella.local>
2025-04-13 14:09:11 -0300

    acerte um numero

---
+++ at_cSharp10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int aleatorio = random.Next(1, 51);
+        int tentativas = 5;
+
+        Console.WriteLine("Tente adivinhar um número entre 1 e 50 Você tem 5 tentativas");
+
+        for (int i = 1; i <= tentativas; i++)
+        {
+            Console.Write($"Tentativa {i}: ");
+            string entrada = Console.ReadLine();
+
+            if (!int.TryParse(entrada, out int chute))
+            {
+                Console.WriteLine("Entrada inválida! Digite um número válido");
+                i--;
+            }
+            else if (chute < 1 || chute > 50)
+            {
+                Console.WriteLine("Fora do intervalo! Digite um número entre 1 e 50");
+                i--;
+            }
+            else
+            {
+                if (chute == aleatorio)
+                {
+                    Console.WriteLine($"Parabéns! Vc acertou o número {aleatorio} na tentativa {i}!");
+                    return;
+                }
+                else if (chute < aleatorio)
+                {
+                    Console.WriteLine("O numero é maior! Tente novamente");
+                }
+                else
+                {
+                    Console.WriteLine("O numero é menor! Tente novamente");
+                }
+            }
+        }
+
+        Console.WriteLine($"Gameover! O numero era {aleatorio}");
+    }
+}
